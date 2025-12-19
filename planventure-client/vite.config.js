b1ff2eb4240dcf10ea32316f22ea0a06d8f0fde3
Jose Antonio Pio <josetonyp@latizana.com>
2025-12-19 11:49:08 +0000

    Publish in docs folder

--- planventure-client/vite.config.js
+++ planventure-client/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: path.join(__dirname, "../client"),
+    outDir: path.join(__dirname, "../docs"),
   },
   esbuild: {
     loader: "jsx",
