af7cee64fe2ffcccfb3f63bb864c0608ea7639b1
Jose Antonio Pio <josetonyp@latizana.com>
2025-12-19 11:58:00 +0000

    Updateing base url for Gtihub pages

--- planventure-client/vite.config.js
+++ planventure-client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/memcoach/",
   plugins: [react()],
   build: {
     outDir: path.join(__dirname, "../docs"),
